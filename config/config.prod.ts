52e16192488aadc80e87c9ab1c04eca3892d4410
joson <joson.chou@outlook.com>
2019-10-09 18:45:45 +0800

    config

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -1,26 +1,19 @@
-import { EggAppInfo } from 'egg';
+
 import { MeYupConfig } from '.';
 
-export default (appInfo: EggAppInfo): MeYupConfig => ({
+export default (): MeYupConfig => ({
   // keys: appInfo.name + '_1553332432157_7228',
+  
   mongoose: {
-    // client: {
-    //   url: 'mongodb://dds-wz9006f21b6cb8a41690-pub.mongodb.rds.aliyuncs.com:3717,dds-wz9006f21b6cb8a42424-pub.mongodb.rds.aliyuncs.com:3717/MeYupShop?replicaSet=mgset-15727591',
-    //   options: {
-    //     poolSize: 20,
-    //     user: 'root',
-    //     pass: 'meyupsrv@mongoDB',
-    //   },
-    // },
     client: {
-      url: 'mongodb://dds-wz9006f21b6cb8a41690-pub.mongodb.rds.aliyuncs.com:3717,dds-wz9006f21b6cb8a42424-pub.mongodb.rds.aliyuncs.com:3717/MeYupTest?replicaSet=mgset-15727591',
+      url: 'mongodb://dds-wz9006f21b6cb8a41690-pub.mongodb.rds.aliyuncs.com:3717,dds-wz9006f21b6cb8a42424-pub.mongodb.rds.aliyuncs.com:3717/MeYupShop?replicaSet=mgset-15727591',
       options: {
         poolSize: 20,
-        // user: 'meyupsrv',
         user: 'root',
         pass: 'meyupsrv@mongoDB',
       },
     },
+
   },
 });
 
